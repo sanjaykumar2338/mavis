234762419004f0655a1573f30e721ce0c74ce8a0
Sanjay Gora <sanjaygora@cwebconsultants.com>
2022-06-30 11:50:55 +0530

    completed files

---
+++ application/logs/log-2022-06-29.php
@@ -0,0 +1,27 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-06-29 10:04:24 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:04:24 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:04:26 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:04:26 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:04:26 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:04:26 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:04:26 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:04:28 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\myworkplace\mavis\application\views\templates\clothesshop\checkout.php 45
+ERROR - 2022-06-29 10:04:28 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:04:28 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:04:28 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:04:28 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:04:28 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:04:35 --> Mailer Error: SMTP connect() failed. https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+ERROR - 2022-06-29 10:04:36 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:04:37 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:04:47 --> mollie_webhook 1
+ERROR - 2022-06-29 10:07:22 --> Severity: error --> Exception: Too few arguments to function Checkout::mollie_payment(), 0 passed in C:\xampp\htdocs\myworkplace\mavis\system\core\CodeIgniter.php on line 532 and exactly 1 expected C:\xampp\htdocs\myworkplace\mavis\application\controllers\Checkout.php 253
+ERROR - 2022-06-29 10:07:47 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:08:00 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:08:05 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:08:05 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:08:11 --> Severity: error --> Exception: Too few arguments to function Checkout::mollie_payment(), 0 passed in C:\xampp\htdocs\myworkplace\mavis\system\core\CodeIgniter.php on line 532 and exactly 1 expected C:\xampp\htdocs\myworkplace\mavis\application\controllers\Checkout.php 253
+ERROR - 2022-06-29 10:09:42 --> 404 Page Not Found: /index
+ERROR - 2022-06-29 10:09:47 --> Severity: error --> Exception: Too few arguments to function Checkout::mollie_payment(), 0 passed in C:\xampp\htdocs\myworkplace\mavis\system\core\CodeIgniter.php on line 532 and exactly 1 expected C:\xampp\htdocs\myworkplace\mavis\application\controllers\Checkout.php 253
---
+++ application/logs/log-2022-06-30.php
@@ -0,0 +1,92 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-06-30 07:30:46 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\myworkplace\mavis\application\views\templates\clothesshop\checkout.php 45
+ERROR - 2022-06-30 07:30:47 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:30:47 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:30:47 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:31:26 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:31:27 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:34:15 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\myworkplace\mavis\application\views\templates\clothesshop\checkout.php 45
+ERROR - 2022-06-30 07:34:15 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:34:15 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:34:15 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:34:15 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:34:15 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:35:27 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\myworkplace\mavis\application\views\templates\clothesshop\checkout.php 46
+ERROR - 2022-06-30 07:35:27 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:35:27 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:35:27 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:35:28 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:35:28 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:37:48 --> Mailer Error: SMTP connect() failed. https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+ERROR - 2022-06-30 07:37:51 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:37:51 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:19 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:19 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:21 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:21 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:24 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:24 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:24 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:24 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:24 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:26 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\myworkplace\mavis\application\views\templates\clothesshop\checkout.php 46
+ERROR - 2022-06-30 07:48:26 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:26 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:26 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:26 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:27 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:37 --> Mailer Error: SMTP connect() failed. https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+ERROR - 2022-06-30 07:48:38 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:38 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:48 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:48 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:50 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 07:48:50 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:06:07 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:06:07 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:06:11 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:06:11 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:06:11 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:06:11 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:06:11 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:06:13 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\myworkplace\mavis\application\views\templates\clothesshop\checkout.php 46
+ERROR - 2022-06-30 08:06:13 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:06:13 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:06:14 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:06:14 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:06:14 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:06:22 --> Mailer Error: SMTP connect() failed. https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+ERROR - 2022-06-30 08:06:23 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:06:24 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:12:41 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:12:41 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:12:45 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:12:45 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:12:45 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:12:45 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:12:45 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:12:47 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\myworkplace\mavis\application\views\templates\clothesshop\checkout.php 46
+ERROR - 2022-06-30 08:12:47 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:12:47 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:12:47 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:12:47 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:12:47 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:12:54 --> Mailer Error: SMTP connect() failed. https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+ERROR - 2022-06-30 08:12:56 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:12:56 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:13:03 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:13:03 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:15:14 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:15:14 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:15:16 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\myworkplace\mavis\application\views\templates\clothesshop\checkout.php 46
+ERROR - 2022-06-30 08:15:16 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:15:16 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:15:16 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:15:16 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:15:16 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:15:24 --> Mailer Error: SMTP connect() failed. https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+ERROR - 2022-06-30 08:15:25 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:15:25 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:15:32 --> 404 Page Not Found: /index
+ERROR - 2022-06-30 08:15:32 --> 404 Page Not Found: /index
